d7e122078c1c65b785577edd45dd5c2b49008b3f
ebruMaytalman <52465630+maytalman@users.noreply.github.com>
2022-03-09 15:18:24 +0300

    Add files via upload

---
+++ java101/1_temelKavramlar/pratik2.java
@@ -0,0 +1,30 @@
+package Giris;
+
+import java.util.Scanner;
+
+public class pratik2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int tutar=0;
+		double kdv=0,kdvli;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Bir tutar giriniz: ");
+		tutar = input.nextInt();
+		
+		if(tutar<1000) {
+			kdv=0.18;
+			kdvli =tutar + (tutar* kdv);
+			
+		}
+		else {
+			kdv=0.08;
+			kdvli = tutar + (tutar* kdv);
+		}
+		System.out.println("KDV'siz tutar: "+tutar);
+		System.out.println("KDV miktari: "+kdv);
+		System.out.println("KDV'li tutar: "+kdvli);
+	}
+
+}
