028289069cb8d9db68a8bf867652e45e540966e9
ebruMaytalman <52465630+maytalman@users.noreply.github.com>
2022-03-10 15:39:38 +0300

    Add files via upload

---
+++ java101/3_donguler/pratik2.java
@@ -0,0 +1,22 @@
+package Giris3;
+
+import java.util.*;
+
+public class pratik2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n,sum=0,count=0;
+		Scanner input = new Scanner(System.in);
+		
+		do {
+			System.out.println("Sayý giriniz: ");
+			n = input.nextInt();
+			if(n%2==0 && n%4 ==0 ) {
+				sum+=n;
+			}
+		}while(n % 2 ==0);
+		System.out.println("toplam "+sum);
+	}
+
+}
