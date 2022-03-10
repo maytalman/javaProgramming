aa025846e576bd562a72c13e92709043ad1f3c1c
ebruMaytalman <52465630+maytalman@users.noreply.github.com>
2022-03-10 14:47:32 +0300

    Update zodyakHesaplama.java

--- java101/2_kosulluIfadeler/zodyakHesaplama.java
+++ java101/2_kosulluIfadeler/zodyakHesaplama.java
@@ -1,7 +1,7 @@
 package Giris2;
 
 import java.util.*;
-
+//Ã‡in ZodyaÄŸÄ± Hesaplama
 public class zodyakHesaplama {
 
 	public static void main(String[] args) {
@@ -9,21 +9,21 @@ public class zodyakHesaplama {
 		int year,zodyak;
 		Scanner input = new Scanner(System.in);
 		
-		System.out.println("Doğum yılınızı girin : ");
+		System.out.println("DoÄ‘um yÃ½lÃ½nÃ½zÃ½ girin : ");
 		year = input.nextInt();
 		
 		zodyak = year%12;
 		switch(zodyak) {
 			case 0: System.out.println("Maymun");break;
 			case 1: System.out.println("Horoz");break;
-			case 2: System.out.println("Köpek");break;
+			case 2: System.out.println("KÃ¶pek");break;
 			case 3: System.out.println("Domuz");break;
 			case 4: System.out.println("Fare");break;
-			case 5: System.out.println("Öküz");break;
+			case 5: System.out.println("Ã–kÃ¼z");break;
 			case 6: System.out.println("Kaplan");break;
-			case 7: System.out.println("Tavşan");break;
+			case 7: System.out.println("TavÅ£an");break;
 			case 8: System.out.println("Ejderha");break;
-			case 9: System.out.println("Yılan");break;
+			case 9: System.out.println("YÃ½lan");break;
 			case 10: System.out.println("At");break;
 			case 11: System.out.println("Koyun");break;
 			}
