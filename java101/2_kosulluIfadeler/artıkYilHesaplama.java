faf6629655172b496e34ba33968065b9fc394d9e
ebruMaytalman <52465630+maytalman@users.noreply.github.com>
2022-03-10 14:56:35 +0300

    Add files via upload

---
+++ java101/2_kosulluIfadeler/artıkYilHesaplama.java
@@ -0,0 +1,24 @@
+package Giris2;
+
+import java.util.*;
+
+//Artýk Yýl Hesaplama
+public class artýkYilHesaplama {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int yil;
+		System.out.println("Yýl giriniz: ");
+		yil = input.nextInt();
+		
+		if(yil %4 ==0) {
+			System.out.println("Artýk yýldýr!");
+		}
+		else if(yil%400 ==0) System.out.println("Artýk yýldýr!");
+		else System.out.println("Artýk bir yýl deðildir!");
+		
+		
+	}
+
+}
