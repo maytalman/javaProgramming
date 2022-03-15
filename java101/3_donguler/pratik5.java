13673b1c8377217bed91145799732d6d12c31fcd
ebruMaytalman <52465630+maytalman@users.noreply.github.com>
2022-03-15 18:46:14 +0300

    Add files via upload

---
+++ java101/3_donguler/pratik5.java
@@ -0,0 +1,23 @@
+package Giris3;
+
+import java.util.*;
+public class pratik5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int taban,us,sonuc=1;
+		Scanner input = new Scanner(System.in);
+		System.out.println("taban sayýyý giriniz: ");
+		taban = input.nextInt();
+		System.out.println("üs sayýyý giriniz: ");
+		us =input.nextInt();
+		
+		int i=0;
+		while(i<us) {
+			sonuc*=taban;
+			i++;
+		}
+		System.out.println(sonuc);
+	}
+
+}
