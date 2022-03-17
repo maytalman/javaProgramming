ec5b21891773e1f99e9f2e94d2fe9bcdc22d2764
ebruMaytalman <52465630+maytalman@users.noreply.github.com>
2022-03-18 01:43:32 +0300

    Add files via upload

---
+++ java101/3_donguler/minMaxBulma.java
@@ -0,0 +1,27 @@
+package Giris3;
+import java.util.*;
+public class minMaxBulma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n,min=0,max=0,i=0,num;
+		Scanner input=new Scanner(System.in);
+		
+		System.out.println("kaç adet sayý girilecek?");
+		n=input.nextInt();
+		
+		while(i<n) {
+			System.out.println("sayý giriniz: ");
+			num = input.nextInt();
+			if(num> max) {
+				max=num;
+			}
+			else if(num<min) {
+				min=num;
+			}
+			i++;
+		}
+		System.out.println("min: "+min+ "\nmax: "+max);
+	}
+
+}
