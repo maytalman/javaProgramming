6df263d1c6394a59670445b47c3a7406968ed69d
ebruMaytalman <52465630+maytalman@users.noreply.github.com>
2022-03-18 02:20:27 +0300

    Update fibonacci.java

--- java101/3_donguler/fibonacci.java
+++ java101/3_donguler/fibonacci.java
@@ -1,21 +1,20 @@
 package Giris3;
 import java.util.*;
 public class fibonacci {
-
+//Fibonacci kodu
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner input = new Scanner(System.in);
+	// TODO Auto-generated method stub
+	Scanner input = new Scanner(System.in);
         int n1=0,n2=1,n3,i,n;
-        System.out.print("N Sayýsýný Girin:");
+        System.out.print("N SayÃ½sÃ½nÃ½ Girin:");
         n = input.nextInt();
         System.out.print(n1+" "+n2);
   
         for(i=2;i<n;++i) {  
-        	
-		   n3=n1+n2;  
-		   System.out.print(" "+n3);  
-		   n1=n2;  
-		   n2=n3;  
+		 n3=n1+n2;  
+		 System.out.print(" "+n3);  
+		 n1=n2;  
+		 n2=n3;  
 		    }
 		 System.out.println();
 		    }
