2d30c7e1e5da35c6b16849cddc9ce230cef46c2c
ebruMaytalman <maytalman216@gmail.com>
2022-03-18 17:00:44 +0300

    Create pratik1.java

---
+++ java101/4_metotlar/pratik1.java
@@ -0,0 +1,31 @@
+
+package Giris4;
+
+import java.util.*;
+public class pratik1 {//2563
+	static boolean isPalindrom(int Num) {
+		int temp=Num,reversNum=0,lastNum;
+		while(temp!=0) {
+			lastNum = temp%10;
+			reversNum = (reversNum * 10) + lastNum;
+			temp = temp/10;
+		}
+		if(Num == reversNum)	return true;
+		else 	return false;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int Num;
+		Scanner input = new Scanner(System.in);
+		System.out.println("Palindromluk kontrolü yapacaðýnýz sayýyý giriniz: ");
+		Num = input.nextInt();
+		
+		if(true == isPalindrom(Num)) {
+			System.out.println("Palindromdur");
+		}
+		else System.out.println("Palindrom deðildir.");
+
+	}
+
+}
