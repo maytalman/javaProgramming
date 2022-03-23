37ba5936ff4bf2204e9b1fc392fb1bb75515aec3
ebruMaytalman <maytalman216@gmail.com>
2022-03-23 23:15:22 +0300

    upgrade

---
+++ java101/5-OOP/maasHesaplamaSistemi/Employee.java
@@ -0,0 +1,66 @@
+package maasHesaplamaSistemi;
+
+public class Employee {
+	String name;
+	double salary;
+	int workHours;
+	int hireYear;
+	
+	public Employee(String name,double salary, int workHours,int hireYear) {
+		this.name= name;
+		this.salary=salary;
+		this.hireYear=hireYear;
+		this.workHours=workHours;
+		
+	}
+	
+	public double tax() {
+		if(this.salary < 1000) {
+			return 0.0;
+		}
+		else {
+			return this.salary*3/100;
+		}
+	}
+	
+	public double bonus() {
+		int fazla;
+		if(this.workHours> 40) {
+			fazla = this.workHours-40;
+			return fazla*30;
+		}
+		else return 0.0;
+	}
+	
+	public double raiseSalary() {
+		int time = this.hireYear- 2021;
+		if(time<10 ) {
+			return this.salary*5/100;
+		}
+		else if(time >9 && time<20){
+			return this.salary*10/100;
+			
+		}
+		else {
+			return this.salary*15/100;
+		}
+	}
+	
+	public double totalSalary() {
+		return this.salary+ tax()+ bonus();
+	}
+	
+	public void printInfo() {
+		
+		System.out.println("********************");
+		System.out.println("Adý:\t"+this.name);
+		System.out.println("Maaþý:\t"+this.salary);
+		System.out.println("Çalýþma Saati:\t"+this.workHours);
+		System.out.println("Baþlangýç Yýlý:\t"+this.hireYear);
+		System.out.println("Vergi:\t"+raiseSalary());
+		System.out.println("Bonus:\t"+bonus());
+		System.out.println("Maaþ Artýþý:\t"+tax());
+		System.out.println("Vergi ve Bonuslar ile birlikte maaþ:\t"+totalSalary());
+		System.out.println("Toplam Maaþ:\t"+(totalSalary()+ raiseSalary()));
+	}
+}
---
+++ java101/5-OOP/maasHesaplamaSistemi/Main.java
@@ -0,0 +1,15 @@
+package maasHesaplamaSistemi;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee e1 = new Employee("ebru", 1100.0,48,2020);
+		e1.printInfo();
+		
+		Employee e2 = new Employee("Beyza ", 4000.0,70,2000);
+		e2.printInfo();
+
+	}
+
+}
