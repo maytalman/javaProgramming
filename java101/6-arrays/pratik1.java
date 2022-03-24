4ac379f8d1baa844cff5deeff149b2a1fda41035
ebruMaytalman <52465630+maytalman@users.noreply.github.com>
2022-03-24 13:53:25 +0300

    Update pratik1.java

--- java101/6-arrays/pratik1.java
+++ java101/6-arrays/pratik1.java
@@ -5,17 +5,13 @@ public class pratik1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int n;
-		double start=0.0;
-		Scanner input=new Scanner(System.in);
-		
-		System.out.println("n sayısını girin: ");
-		n = input.nextInt();
-		
-		for(int i=1;i<=n;i++) {
-			start = (1.0/i)+ start;
-		}
-		System.out.println(start);
+		int[] numbers = {1, 2, 3, 4, 5};
+	        double sum = 0.0;
+	        for (int i = 0; i < numbers.length; i++) {
+	        	sum = (1.0/numbers[i])+ sum;
+	        }
+
+	        System.out.println(sum );
 	}
 
 }
