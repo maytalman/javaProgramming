e53bcbdc3e21974868da760f17579e30aaf4940a
ebruMaytalman <52465630+maytalman@users.noreply.github.com>
2022-03-24 15:19:12 +0300

    Add files via upload

---
+++ java101/6-arrays/pratik3.java
@@ -0,0 +1,31 @@
+package Giris5;
+
+public class pratik3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[][] letter = new String[6][4];
+
+        for (int i = 0; i < letter.length; i++) {
+            for (int j = 0; j < letter[i].length; j++) {
+                if (i == 0 || i == 2 || i==5) {
+                    letter[i][j] = " * ";
+                } else if (j == 0 || j == 3) {
+                    letter[i][j] = " * ";
+                } else {
+                    letter[i][j] = "   ";
+                }
+            }
+        }
+
+        for (String[] row : letter){
+            for (String col : row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+
+	}
+
+
